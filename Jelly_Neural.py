f6f660deb41c72eef9d882897d081c6b85d505bb
Ryan <rgbarishian@gmail.com>
2020-04-19 15:06:16 -0500

    Update Imagegenerator for more variation

--- Jelly_Neural.py
+++ Jelly_Neural.py
@@ -16,13 +16,31 @@ from time import time
 from keras.utils import plot_model
 
 #train_path = '/home/ryan/Documents/Galaxy_Zoo/Jell_Test/Grayscale/Train_Valid_Mix'#mixed train path
-train_path = '/home/ryan/Documents/Galaxy_Zoo/Jell_Test/Grayscale/Train_Independent'
-valid_path = '/home/ryan/Documents/Galaxy_Zoo/Jell_Test/Grayscale/Valid'
-test_path = '/home/ryan/Documents/Galaxy_Zoo/Jell_Test/Test'
+train_path = '/home/ryan/Documents/DATA/CNN/Denoised/Train'
+valid_path = '/home/ryan/Documents/DATA/CNN/Denoised/Valid'
+test_path = '/home/ryan/Documents/DATA/Galaxy_Zoo/SmallBatch_TestBW'
 
-train_batches = ImageDataGenerator(rescale=1/255).flow_from_directory(train_path, target_size=(424,424), classes=['Not_Jellies','Poss_Jellies'], batch_size=35)
-valid_batches = ImageDataGenerator(rescale=1/255).flow_from_directory(valid_path, target_size=(424,424), classes=['Not_Jellies','Poss_Jellies'], batch_size=11)
-test_batches = ImageDataGenerator(rescale=1/255).flow_from_directory(test_path, target_size=(424,424), batch_size=15)
+train_batches = ImageDataGenerator(rescale=1/255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        rotation_range=360,
+        width_shift_range=.25,
+        height_shift_range=.25,
+        fill_mode='wrap').flow_from_directory(train_path, target_size=(424,424), classes=['Not_Jellies','Jellies'], batch_size=35)
+valid_batches = ImageDataGenerator(rescale=1/255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        rotation_range=360,
+        width_shift_range=.15,
+        height_shift_range=.15,
+        fill_mode='wrap').flow_from_directory(valid_path, target_size=(424,424), classes=['Not_Jellies','Jellies'], batch_size=29)
+test_batches = ImageDataGenerator(rescale=1/255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        rotation_range=360,
+        width_shift_range=.15,
+        height_shift_range=.15,
+        fill_mode='wrap').flow_from_directory(test_path, target_size=(424,424), classes=['Not_Jellies','Jellies'], batch_size=22)
 
 #LOAD PRE-TRAINED
 """
@@ -97,8 +115,8 @@ model.add(Dense(2, activation='softmax'))
 model = Sequential()
 model.add(Conv2D(64,(3,3), input_shape=(424,424,3), activation='relu'))#3x3 is default
 model.add(MaxPooling2D(pool_size=(3,3)))
-#model.add(Dropout(.1))#test
-model.add(Dense(32, activation='relu'))#test
+model.add(Dropout(.1))#test
+model.add(Dense(32, activation='relu'))
 model.add(Conv2D(64,(3,3), activation='relu'))#input_shape=(424,424,3)
 model.add(MaxPooling2D(pool_size=(3,3)))
 model.add(Dense(64, activation='relu'))
@@ -131,7 +149,7 @@ model.compile(optimizer=myoptimizer, loss = 'mean_squared_error', metrics=['accu
 #monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=5, verbose=1)
 tensorboard= TensorBoard(log_dir="logs/{}".format(time()))
 #tensorboard.add_graph(tf.summary.histogram)
-model.fit_generator(train_batches, steps_per_epoch=3, validation_data=valid_batches, validation_steps=2, epochs=25, verbose=2, callbacks=[tensorboard])
+model.fit_generator(train_batches, steps_per_epoch=5, validation_data=valid_batches, validation_steps=2, epochs=35, verbose=2, callbacks=[tensorboard])
 
 """
 #Save Weights and model
@@ -141,7 +159,14 @@ with open("model_in_json.json", "w") as json_file:
 model.save_weights('simple_CNN.h5')
 """
 
-model.save('simple_CNN.h5')
+model.save_weights('Weights_CNN.h5')
+model.save('Model_CNN.h5')
 #Plot graph of accuracies
 #plt.plot()
 
+#test file
+# filenames = train_batches.filenames
+# nb_samples = len(filenames)
+# predict = model.predict_generator(test_batches, steps=1 )
+# print(predict)
+# np.savetxt('test.csv', predict)
